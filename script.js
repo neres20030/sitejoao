bd908a50e67ae3520b5cedac973024a744d19819
João Paulo <joaopaulo.lealneres@gmail.com>
2024-11-02 02:47:15 -0300

    NEW COMMIT

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ function toggleMode() {
     const html = document.documentElement;
     html.classList.toggle("light");
 
-    const img = document.querySelector("#profile img"); 
+    const img = document.querySelector("#profile img");
     if (html.classList.contains("light")) {
         img.setAttribute("src", "./assets/WhatsApp Image 2024-10-25 at 7.40.05 PM.png");
     } else {
         img.setAttribute("src", "./assets/WhatsApp Image 2024-10-25 at 7.40.05 PM.png");
     }
+
+    fetch('https://api.github.com/users/neres20030/repos')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // Aqui você pode manipular os dados para exibir no seu portfólio
+        })
+        .catch(error => console.error('Erro:', error));
 }
